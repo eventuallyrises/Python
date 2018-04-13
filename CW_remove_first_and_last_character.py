fab083f79cf8dd58d3eb84198fb4a14e7cce1b76
Michael Powers <36204755+eventuallyrises@users.noreply.github.com>
2018-04-12 22:43:24 -0400

    CodeWars Kata

---
+++ CW_remove_first_and_last_character.py
@@ -0,0 +1,17 @@
+#remove the first and last letters of a string
+
+def remstr(wstring):
+    if len(wstring)<3:
+        print("Not enough chars mang.")
+    else:
+        print(wstring[1:-1])
+
+
+
+
+
+
+'''take in a string
+index the string, return a string that is minus the first and last index?'''
+
+remstr("Testing attention please!")
